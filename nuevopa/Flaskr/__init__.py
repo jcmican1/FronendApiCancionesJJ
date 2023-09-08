63cd063fb59c70035051deb6a98bae4a3e2e3ee0
jcmican1 <119264781+jcmican1@users.noreply.github.com>
2023-09-08 11:12:41 -0500

    .

--- nuevopa/Flaskr/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import Flask
-from flask_cors import CORS
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3307/tutorial_canciones'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    CORS(app)
-    return app
